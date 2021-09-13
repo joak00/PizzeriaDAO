bdb75207ca876f9a7c97a59c8aee88e3ef2936db
Joaquín J. Martínez <65954583+joak00@users.noreply.github.com>
2021-09-13 20:46:34 +0200

    Delete Ejercicios directory

--- Ejercicios/Ejercicio2.sql
+++
@@ -1,4 +0,0 @@
-SELECT pizza.name, SUM(ingredient.price) AS precio FROM pizza
-INNER JOIN pizza_ingredient ON pizza.id = pizza_ingredient.id_pizza
-INNER JOIN ingredient ON pizza_ingredient.id_ingredient = ingredient.id
-GROUP BY pizza.name;
--- Ejercicios/Ejercicio4.sql
+++
@@ -1,3 +0,0 @@
-SELECT pizza.id AS IdPizzaSinIngredientes, pizza.name AS PizzaNameSinIngredientes
-FROM pizza LEFT JOIN pizza_ingredient on Pizza.ID = pizza_ingredient.id_pizza 
-WHERE pizza_ingredient.id_ingredient IS NULL;
